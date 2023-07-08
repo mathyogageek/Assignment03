347120d1a12dd950f68f244f0f8ab8d73bc2ee2f
Rie Wilsterman <riewilsterman@Mac-mini.local>
2023-07-07 20:24:51 -0700

    Update  07-ValidateNumeric-Entries.js

--- beginning/js/07-ValidateNumeric-Entries.js
+++ beginning/js/07-ValidateNumeric-Entries.js
@@ -1,19 +1,19 @@
 // 07-ValidateNumeric-Entries.js
 
 
-// Prompt for investment amount and validate
+// Prompt for validate investment amount 
 var investment = prompt("Enter investment amount as xxxx.xx");
 while (isNaN(parseFloat(investment)) || parseFloat(investment) < 0) {
     investment = prompt("Invalid input! Enter investment amount as xxxx.xx");
 }
 
-// Prompt for interest rate and validate
+// Prompt for validate interest rate 
 var rate = prompt("Enter interest rate as xx.x");
 while (isNaN(parseFloat(rate)) || parseFloat(rate) < 0 || parseFloat(rate) > 100) {
     rate = prompt("Invalid input! Enter interest rate as xx.x");
 }
 
-// Prompt for number of years and validate
+// Prompt for validate number of years 
 var years = prompt("Enter number of years");
 while (isNaN(parseInt(years)) || parseInt(years) < 1 || parseInt(years) > 30) {
     years = prompt("Invalid input! Enter number of years between 1 and 30 Integers!");
@@ -21,7 +21,7 @@ while (isNaN(parseInt(years)) || parseInt(years) < 1 || parseInt(years) > 30) {
 
 var futureValue = investment * Math.pow(1 + rate / 100, years);
 
-// Output the validated values
+// Display the validated values
 console.log("Validated Investment Amount:", investment);
 console.log("Validated Interest Rate:", rate);
 console.log("Validated Number of Years:", years);
